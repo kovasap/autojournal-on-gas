ecf4f5cf7758c3df385ae2e1365bf81116557627
Kovas Palunas <kovas.palunas@gmail.com>
2022-05-14 13:00:20 -0700

    some notes

--- project.clj
+++ project.clj
@@ -7,6 +7,10 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [
+                 ; These older versions of clojure and clojurescript are
+                 ; necessary for some reason with this build configuration. If
+                 ; I use the newer ones in shadow-cljs.edn, I get errors when I
+                 ; try to push to google apps script.
                  [org.clojure/clojure "1.9.0"]
                  [org.clojure/clojurescript "1.10.439"]]
   :cljsbuild {:builds [{
