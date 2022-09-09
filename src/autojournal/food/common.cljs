74a145f9ba297c7e7144c859454771a74e61ade0
Kovas Palunas <kovas.palunas@gmail.com>
2022-09-08 21:12:42 -0700

    some reformatting

--- src/autojournal/food/common.cljs
+++ src/autojournal/food/common.cljs
@@ -1,6 +1,6 @@
 (ns autojournal.food.common
   (:require [inflections.core :refer [singular]]))
-             
+
 
 ; TODO Make this reference a sheet name
 (def nutrient-targets-sheet-id
@@ -12,22 +12,20 @@
 
 (def DAYS-TO-SUMMARIZE 5)
 
-
 (def FoodName :string)
 (def NutrientName :string)
 (def Food
   [:map [:quantity :double]
-        [:quantity-units :string]
-        [:name FoodName]
-        [:category :string]
-        [:nutrients [:map-of NutrientName :double]]])
+   [:quantity-units :string]
+   [:name FoodName]
+   [:category :string]
+   [:nutrients [:map-of NutrientName :double]]])
 
 (def Meal
   [:map [:datetime :any]  ; js datetime
-        [:foods [:sequential Food]]
-        [:oil [:enum "None" "Light" "Medium" "Heavy"]]
-        [:picture :string]]) 
-
+   [:foods [:sequential Food]]
+   [:oil [:enum "None" "Light" "Medium" "Heavy"]]
+   [:picture :string]])
 
 (def units-map
   {"cup" ["cups"]
@@ -38,7 +36,6 @@
    "small" []
    "medium" []
    "large" []})
-
 
 (defn -singular-fixed
   [s]
--- src/autojournal/food/main.cljs
+++ src/autojournal/food/main.cljs
@@ -45,76 +45,76 @@
 (def t3 #inst "2022-08-11T19:12:06.700-00:00")
 (def t4 #inst "2022-08-11T19:20:21.938-00:00")
 
-(def test-food-db 
+(def test-food-db
   {"potatoes russet flesh and skin baked"
    {"merium"
-    {:name "Potatoes, Russet, Flesh and Skin, Baked",
-     :quantity 1,
+    {:name "Potatoes, Russet, Flesh and Skin, Baked"
+     :quantity 1
      :category "roots"
-     :quantity-units "medium",
-     :nutrients {"Energy (kcal)" 164.35, "Carbs (g)" 37.09}}},
+     :quantity-units "medium"
+     :nutrients {"Energy (kcal)" 164.35, "Carbs (g)" 37.09}}}
    "potatoes"
    {"medium"
-    {:name "Potatoes, Russet, Flesh and Skin, Baked",
-     :quantity 1,
+    {:name "Potatoes, Russet, Flesh and Skin, Baked"
+     :quantity 1
      :category "roots"
-     :quantity-units "medium",
+     :quantity-units "medium"
      :nutrients {"Energy (kcal)" 164.35, "Carbs (g)" 37.09}}}
    "steamed rice"
    {"g"
-    {:name "White Rice, steamed",
-     :quantity 100,
+    {:name "White Rice, steamed"
+     :quantity 100
      :category "grains"
-     :quantity-units "g",
-     :nutrients {"Energy (kcal)" 100, "Carbs (g)" 50}},
+     :quantity-units "g"
+     :nutrients {"Energy (kcal)" 100, "Carbs (g)" 50}}
     "cup"
-    {:name "White Rice, steamed",
-     :quantity 1,
+    {:name "White Rice, steamed"
+     :quantity 1
      :category "grains"
-     :quantity-units "cup",
-     :nutrients {"Energy (kcal)" 100, "Carbs (g)" 50}}},
+     :quantity-units "cup"
+     :nutrients {"Energy (kcal)" 100, "Carbs (g)" 50}}}
    "rice"
    {"g"
-    {:name "White Rice, steamed",
-     :quantity 100,
+    {:name "White Rice, steamed"
+     :quantity 100
      :category "grains"
-     :quantity-units "g",
-     :nutrients {"Energy (kcal)" 100, "Carbs (g)" 50}},
+     :quantity-units "g"
+     :nutrients {"Energy (kcal)" 100, "Carbs (g)" 50}}
     "cup"
-    {:name "White Rice, steamed",
-     :quantity 1,
+    {:name "White Rice, steamed"
+     :quantity 1
      :category "grains"
-     :quantity-units "cup",
+     :quantity-units "cup"
      :nutrients {"Energy (kcal)" 100, "Carbs (g)" 50}}}
    "cauliflower"
    {"cup"
-    {:name "Cauliflower",
-     :quantity 1,
+    {:name "Cauliflower"
+     :quantity 1
      :category "Vegetable"
-     :quantity-units "cup",
+     :quantity-units "cup"
      :nutrients {"Energy (kcal)" 50, "Carbs (g)" 0}}}
    "carrot"
    {"unit"
-    {:name "Carrot",
-     :quantity 1,
+    {:name "Carrot"
+     :quantity 1
      :category "Vegetable"
-     :quantity-units "unit",
+     :quantity-units "unit"
      :nutrients {"Energy (kcal)" 30, "Carbs (g)" 0}}}
    "bell pepper"
    {"cup"
-    {:name "Bell Pepper",
-     :quantity 1,
+    {:name "Bell Pepper"
+     :quantity 1
      :category "Vegetable"
-     :quantity-units "cup",
+     :quantity-units "cup"
      :nutrients {"Energy (kcal)" 10, "Carbs (g)" 0}}}})
 
 (node-only
-  #(gmail/send-self-mail "Daily Report"
-     (build-report-email
-       (add-db-data-to-meals
-         (map row->meal
-              '({:Timestamp t1, :Foods "one cup cauliflower \n 1/2 cup steamed rice\n", (keyword "Oil Amount") "None", :Picture "", :Picture.http "", :__id "TWZbOlhGSGszUkRPJUgobkBzTUQ"}
-                {:Timestamp t2, :Foods "half cup rice\none cup cauliflower\none cup tomatoes\n one carrot\n1/2 cup bell pepper\n1/2 cup olives", (keyword "Oil Amount") "None", :Picture "", :Picture.http "", :__id "UHh3ZVRpO3cjPG90OWcpKFAjbGI"}
-                {:Timestamp t3, :Foods "half cup rice", (keyword "Oil Amount") "None", :Picture "", :Picture.http "", :__id "IzFzV1lKenM6Rz5ANmsoTExdODY"}
-                {:Timestamp t4, :Foods "", (keyword "Oil Amount") "None", :Picture "", :Picture.http "", :__id "R1QmJiF1Om8zMFsyZE4hQnRKUj4"}))
-         test-food-db))))
+ #(gmail/send-self-mail "Daily Report"
+                        (build-report-email
+                         (add-db-data-to-meals
+                          (map row->meal
+                               '({:Timestamp t1, :Foods "one cup cauliflower \n 1/2 cup steamed rice\n", (keyword "Oil Amount") "None", :Picture "", :Picture.http "", :__id "TWZbOlhGSGszUkRPJUgobkBzTUQ"}
+                                 {:Timestamp t2, :Foods "half cup rice\none cup cauliflower\none cup tomatoes\n one carrot\n1/2 cup bell pepper\n1/2 cup olives", (keyword "Oil Amount") "None", :Picture "", :Picture.http "", :__id "UHh3ZVRpO3cjPG90OWcpKFAjbGI"}
+                                 {:Timestamp t3, :Foods "half cup rice", (keyword "Oil Amount") "None", :Picture "", :Picture.http "", :__id "IzFzV1lKenM6Rz5ANmsoTExdODY"}
+                                 {:Timestamp t4, :Foods "", (keyword "Oil Amount") "None", :Picture "", :Picture.http "", :__id "R1QmJiF1Om8zMFsyZE4hQnRKUj4"}))
+                          test-food-db))))
